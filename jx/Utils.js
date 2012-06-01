a57a2b5a402e130a58d8aa797ebde3c374834a4a
alaingilbert <alain.gilbert.15@gmail.com>
2012-05-31 23:38:49 -0400

    add some utils functions

--- jx/Utils.js
+++ jx/Utils.js
@@ -5,6 +5,26 @@ goog.provide('jx.Utils');
 jx.Utils = {};
 
 
+/**
+ * @param {number} min Minimum value (included).
+ * @param {number} max Maximum value (included).
+ * @return {number} A random number.
+ */
+jx.Utils.rand = function(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+
+/**
+ * @param {number} min Minimum value (included).
+ * @param {number} max Maximum value (included).
+ * @return {number} A random floating number.
+ */
+jx.Utils.randf = function(min, max) {
+  return Math.random() * (max - min + 1) + min;
+};
+
+
 /** */
 jx.Utils.requestAnimFrame = (function() {
   return window.requestAnimationFrame ||       // La forme standardisée
